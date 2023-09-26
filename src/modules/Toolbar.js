9e13bda3e461c59f36bbcbe8f3d057ec5c00692d
maxime <maxime.arretche@gmail.com>
2023-09-26 19:23:46 +0200

    feat: add inline positioning

--- src/modules/Toolbar.js
+++ src/modules/Toolbar.js
@@ -1,13 +1,15 @@
 import Quill from "quill";
-import IconAlignLeft from 'quill/assets/icons/align-left.svg';
-import IconAlignCenter from 'quill/assets/icons/align-center.svg';
-import IconAlignRight from 'quill/assets/icons/align-right.svg';
+import IconAlignLeft from 'quill/assets/icons/float-left.svg';
+import IconAlignCenter from 'quill/assets/icons/float-center.svg';
+import IconAlignRight from 'quill/assets/icons/float-right.svg';
+import IconAlignInline from '../assets/float-inline.svg';
 import { BaseModule } from './BaseModule';
 
 const Parchment = Quill.imports.parchment;
 const FloatStyle = new Parchment.Attributor.Style('float', 'float');
 const MarginStyle = new Parchment.Attributor.Style('margin', 'margin');
 const DisplayStyle = new Parchment.Attributor.Style('display', 'display');
+const VerticalAlignStyle = new Parchment.Attributor.Style('vertical-align', 'vertical-align');
 
 const offsetAttributor = new Parchment.Attributor.Attribute('nameClass', 'class', {
 	scope: Parchment.Scope.INLINE,
@@ -40,6 +42,7 @@ export class Toolbar extends BaseModule {
                 apply: () => {
                     DisplayStyle.add(this.img, 'inline');
                     FloatStyle.add(this.img, 'left');
+									VerticalAlignStyle.remove(this.img);
 					MarginStyle.add(this.img, '0 1em 1em 0');
 					this.img.align = 'left';
                 },
@@ -50,6 +53,7 @@ export class Toolbar extends BaseModule {
                 apply: () => {
                     DisplayStyle.add(this.img, 'block');
                     FloatStyle.remove(this.img);
+									VerticalAlignStyle.remove(this.img);
 					MarginStyle.add(this.img, 'auto');
 					this.img.align = 'center';
                 },
@@ -60,11 +64,23 @@ export class Toolbar extends BaseModule {
                 apply: () => {
                     DisplayStyle.add(this.img, 'inline');
                     FloatStyle.add(this.img, 'right');
+										VerticalAlignStyle.remove(this.img);
 					MarginStyle.add(this.img, '0 0 1em 1em');
 					this.img.align = 'right';
                 },
                 isApplied: () => FloatStyle.value(this.img) == 'right',
             },
+						{
+							icon: IconAlignInline,
+							apply: () => {
+								DisplayStyle.add(this.img, 'inline');
+								FloatStyle.add(this.img, 'none');
+								VerticalAlignStyle.add(this.img, 'text-bottom');
+								MarginStyle.add(this.img, '0');
+								this.img.align = '';
+							},
+							isApplied: () => FloatStyle.value(this.img) == 'none',
+						}
         ];
     };
 
@@ -82,6 +98,8 @@ export class Toolbar extends BaseModule {
 					FloatStyle.remove(this.img);
 					MarginStyle.remove(this.img);
 					DisplayStyle.remove(this.img);
+					VerticalAlignStyle.remove(this.img);
+					this.img.align = '';
 				}else {
 						// otherwise, select button and apply
 					this._selectButton(button);
